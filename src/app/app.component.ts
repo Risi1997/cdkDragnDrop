b7ec0f6e7097978ff975752dbcc169883f296aa0
Ritesh Saxena <riteshsaxena1998@gmail.com>
2021-03-17 10:31:59 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {CdkDragDrop} from '@angular/cdk/drag-drop';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+  // the array of 64 items.
   items=[
     {value:1, color: 'red'}, {value:2, color: 'green'}, {value:3, color: 'yellow'}, {value:4, color: 'blue'},
     {value:5, color: 'black'}, {value:6, color: 'pink'},{value:7, color: 'brown'},  {value:8, color: 'orange'},
@@ -32,7 +33,16 @@ export class AppComponent {
     {value:57, color: 'red'},{value:58, color: 'green'},{value:59, color: 'yellow'},{value:60, color: 'blue'},
     {value:61, color: 'black'},{value:62, color: 'pink'},{value:63, color: 'brown'},{value:64, color: 'orange'}
   ];
-
+  
+  /** this method uses CdkDragDrop is a event which is emitted when the user drag or drops a div inside the drop zone. The cdkGroupList on html connects multiple node items to other node items and groups them inside the drop zone for the elements to be dropped.
+   cdkDropList is the container in which the item was dropped.
+   cdkDropListOrientation defines the horizontal/vertical orientation of the list.
+   cdkDropListDropped event when the list is dropped in drop zone.
+   cdkDrag defines item that is being dropped.
+   cdkDragPlaceholder element that will be used as a template for the placeholder of a CdkDrag when it is being dragged.
+   previousContaniner is the container from which the item was picked up.
+   contaniner is the container on which the item will be dropped.
+  */
   drop(event: CdkDragDrop<any>) {
     this.items[event.previousContainer.data.index]=event.container.data.item
     this.items[event.container.data.index]=event.previousContainer.data.item
