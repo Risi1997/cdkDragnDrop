d8b83a94e2dbadc267862484e90231af14dcdb7f
Ritesh Saxena <riteshsaxena1998@gmail.com>
2021-03-17 10:10:47 +0530

    CDK DragnDrop
    Implementation drag n drop of 8*8 board with 8 different colors.

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, } from '@angular/core';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+  items=[
+    {value:1, color: 'red'}, {value:2, color: 'green'}, {value:3, color: 'yellow'}, {value:4, color: 'blue'},
+    {value:5, color: 'black'}, {value:6, color: 'pink'},{value:7, color: 'brown'},  {value:8, color: 'orange'},
+
+    {value:9, color: 'red'}, {value:10, color: 'green'}, {value:11, color: 'yellow'}, {value:12, color: 'blue'},
+    {value:13, color: 'black'}, {value:14, color: 'pink'}, {value:15, color: 'brown'}, {value:16, color: 'orange'},
+
+    {value:17, color: 'red'}, {value:18, color: 'green'}, {value:19, color: 'yellow'}, {value:20, color: 'blue'},
+    {value:21, color: 'black'}, {value:22, color: 'pink'}, {value:23, color: 'brown'}, {value:24, color: 'orange'},
+
+    {value:25, color: 'red'}, {value:26, color: 'green'}, {value:27, color: 'yellow'}, {value:28, color: 'blue'},
+    {value:29, color: 'black'}, {value:30, color: 'pink'}, {value:31, color: 'brown'}, {value:32, color: 'orange'},
+
+    {value:33, color: 'red'}, {value:34, color: 'green'}, {value:35, color: 'yellow'},{value:36, color: 'blue'},
+    {value:37, color: 'black'}, {value:38, color: 'pink'}, {value:39, color: 'brown'},{value:40, color: 'orange'},
+
+    {value:41, color: 'red'}, {value:42, color: 'green'}, {value:43, color: 'yellow'},{value:44, color: 'blue'},
+    {value:45, color: 'black'}, {value:46, color: 'pink'}, {value:47, color: 'brown'},{value:48, color: 'orange'},
+    
+    {value:49, color: 'red'},{value:50, color: 'green'},{value:51, color: 'yellow'},{value:52, color: 'blue'},
+    {value:53, color: 'black'},{value:54, color: 'pink'},{value:55, color: 'brown'},{value:56, color: 'orange'},
+
+    {value:57, color: 'red'},{value:58, color: 'green'},{value:59, color: 'yellow'},{value:60, color: 'blue'},
+    {value:61, color: 'black'},{value:62, color: 'pink'},{value:63, color: 'brown'},{value:64, color: 'orange'}
+  ];
+
+  drop(event: CdkDragDrop<any>) {
+    this.items[event.previousContainer.data.index]=event.container.data.item
+    this.items[event.container.data.index]=event.previousContainer.data.item
+    event.currentIndex=0;
+    console.log(event.previousContainer.data,'-->',event.container.data)
+  }
+}
